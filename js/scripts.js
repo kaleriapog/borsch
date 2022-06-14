991f3bb670d8072f069b3ff56a837f3bb8b2b095
kaleriapog <pogrelova.kaleria@gmail.com>
2022-06-14 21:10:37 +0300

    add partially product page

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,6 @@ const tabNavigationVegan = document.getElementById('tab-navigation-vegan')
 const tabProductMeat = document.getElementById('tab-product-meat')
 const tabProductVegan = document.getElementById('tab-product-vegan')
 const accordion = document.querySelector('.accordion-list')
-const items = accordion.querySelectorAll('.accordion-list-item')
 const ourDetailsIconClose = document.querySelector('.our-details-icon-close')
 const ourDetailsBlock = document.querySelector('.our-details')
 const ourDetailsTrigger = document.querySelectorAll('.open-our-details-trigger');
@@ -30,52 +29,67 @@ if(tabNavigationMeat) {
 }
 
 // for reviews slider
-const swiperReviews = new Swiper('.reviews__slider', {
-    direction: 'horizontal',
-    loop: true,
-    slidesPerView: "auto",
-    breakpoints: {
-        320: {
-            spaceBetween: 15,
+if(document.querySelector('.reviews__slider')) {
+    const swiperReviews = new Swiper('.reviews__slider', {
+        direction: 'horizontal',
+        loop: true,
+        slidesPerView: "auto",
+        breakpoints: {
+            320: {
+                spaceBetween: 15,
+            },
+            767: {
+                spaceBetween: 32,
+            },
         },
-        767: {
-            spaceBetween: 32,
+        centeredSlides: true,
+        navigation: {
+            nextEl: '.reviews__button-next',
+            prevEl: '.reviews__button-prev',
         },
-    },
-    centeredSlides: true,
-    navigation: {
-        nextEl: '.reviews__button-next',
-        prevEl: '.reviews__button-prev',
-    },
-})
-
+    })
+}
 // for media slider
-const swiperMedia = new Swiper('.media-about-us__slider', {
-    direction: 'horizontal',
-    loop: true,
-    centeredSlides: true,
-    breakpoints: {
-        320: {
-            spaceBetween: 15,
-            slidesPerView: 1,
+if(document.querySelector('.media-about-us__slider')) {
+    const swiperMedia = new Swiper('.media-about-us__slider', {
+        direction: 'horizontal',
+        loop: true,
+        centeredSlides: true,
+        breakpoints: {
+            320: {
+                spaceBetween: 15,
+                slidesPerView: 1,
+            },
+            420: {
+                spaceBetween: 15,
+                slidesPerView: 2,
+            },
+            600: {
+                slidesPerView: 3,
+                spaceBetween: 30,
+            },
         },
-        420: {
-            spaceBetween: 15,
-            slidesPerView: 2,
+        navigation: {
+            nextEl: '.media-about-us__button-next',
+            prevEl: '.media-about-us__button-prev',
         },
-        600: {
-            slidesPerView: 3,
-            spaceBetween: 30,
+    })
+}
+
+//for product template images
+if(document.querySelector('.slider-product-images')) {
+    const swiperProductTemplateImages = new Swiper(".slider-product-images", {
+        pagination: {
+            el: ".swiper-pagination",
+            dynamicBullets: true,
         },
-    },
-    navigation: {
-        nextEl: '.media-about-us__button-next',
-        prevEl: '.media-about-us__button-prev',
-    },
-})
+    });
+}
 
 // for accordion
-if(document.querySelector('.accordion-list')) {
+if(accordion) {
+    const items = accordion.querySelectorAll('.accordion-list-item')
+
     function toggleAccordion() {
         let thisItem = this.closest('.accordion-list-item');
 
@@ -95,6 +109,7 @@ if(document.querySelector('.accordion-list')) {
 
     items.forEach(question => question.addEventListener('click', toggleAccordion))
 }
+
 //for open our-details
 if(ourDetailsTrigger) {
     ourDetailsTrigger.forEach((elem) => {
